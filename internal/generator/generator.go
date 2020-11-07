8ecab9f1161c222888bc3db1222a37286c31f702
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2020-11-07 17:33:20 -0500

    Update generator.go
    added comments mainly

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -16,17 +16,17 @@ type item struct {
 type location struct {
 	name      string
 	address   string
-	inventory map[*item]int
-	items     []*item
+	inventory map[*item]int //stores the item and quantity, and eventually sales data
+	items     []*item       //I have this in addition to the map so that items in a location can eventually be sorted
 	owed      float32
 }
 
-func (l *location) Show() {
+func (l *location) Show() { //shows current information for location
 	fmt.Println(l.name)
 
 	fmt.Println("\nCurrent inventory:")
 
-	for _, item := range l.items {
+	for _, item := range l.items { //again, this will eventually be sorted
 		fmt.Println(item.name, ": ", l.inventory[item])
 	}
 
@@ -40,7 +40,7 @@ func NewItem(name string, cat *category, cost float32, price float32) *item {
 	return a
 }
 
-func NewLocation(name, address string) *location {
+func NewLocation(name, address string) *location { //creates a new location to populate with items
 	m := make(map[*item]int)
 	l := make([]*item, 0)
 
@@ -49,16 +49,16 @@ func NewLocation(name, address string) *location {
 	return a
 }
 
-func NewCategory(tax float32) *category {
-	a := &category{tax}
+func NewCategory(tax float32) *category { //in case you want to track data for a certain group of items
+	a := &category{tax} //this isn't really necessary for retail consignment in PA, but what the heck
 
 	return a
 }
 
-func containsItem(l []*item, x *item) bool {
+func containsItem(l []*item, x *item) bool { //checks whether a location has been stocked with an item
 	contains := false
 
-	for _, i := range l {
+	for _, i := range l { //if the item is found in the slice, return "true"
 		if i == x {
 			contains = true
 		}
@@ -66,14 +66,14 @@ func containsItem(l []*item, x *item) bool {
 	return contains
 }
 
-func (l *location) AddItem(x *item, y int) *location {
-	contains := containsItem(l.items, x)
+func (l *location) AddItem(x *item, y int) *location { //adds inventory to the location, unfortunately one item at a time currently
+	contains := containsItem(l.items, x) //check to see if the item is currently stocked
 
 	switch contains {
 	case true:
-		l.inventory[x] += y
+		l.inventory[x] += y //increase the inventory count if the item exists at the store
 	default:
-		l.items = append(l.items, x)
+		l.items = append(l.items, x) //if not, add the item to the list of existing items first
 		l.inventory[x] = y
 	}
 
@@ -81,7 +81,7 @@ func (l *location) AddItem(x *item, y int) *location {
 
 }
 
-func (l *location) TakeStock(x *item, y int) *location {
+func (l *location) TakeStock(x *item, y int) *location { //sold is original inventory - current inventory
 	sold := l.inventory[x] - y
 	l.owed += float32(sold) * x.price
 
