48b1ca67f76e8eda693ce451c91ffa0ac23f01fd
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2020-11-13 13:01:55 -0500

    Created web
    Started tinkering with the web framework. Haven't started on getting it interacting with the database yet, as I'm just trying to refresh myself in general on web dev. Once I get the wheels turning, I'll actually start integrating

---
+++ web/main/webtesting.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Index!")
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/static/home.html")
+}
+
+func main() {
+
+	fmt.Println("Now serving on port 8080")
+
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/home", handleHome)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
