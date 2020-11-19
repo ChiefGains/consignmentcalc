43b196a81d1587667681aca96335a68c4e274de3
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2020-11-19 18:51:28 -0500

    Updated some stuff
    fixed the directory tree and calls so that I could finally get the stupid css to work with the stupid html. Now I need to *actually* start designing pages so that I can get to the logic part

--- web/main/webtesting.go
+++ web/main/webtesting.go
@@ -18,7 +18,9 @@ func main() {
 
 	fmt.Println("Now serving on port 8080")
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+	static := http.StripPrefix("/static/", http.FileServer(http.Dir("web/static")))
+
+	http.Handle("/static/", static)
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/home", handleHome)
 
